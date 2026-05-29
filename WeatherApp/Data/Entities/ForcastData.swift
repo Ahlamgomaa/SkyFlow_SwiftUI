361d78c44603b808320319a410764212f1c2ba13
Ahlam <ahlamgomaa304@gmail.com>
2026-05-29 16:50:01 +0300

    fix issue

--- WeatherApp/Data/Entities/ForcastData.swift
+++ WeatherApp/Data/Entities/ForcastData.swift
@@ -1,10 +1,10 @@
 
 
-public struct ForecastData: Decodable, Sendable {
+public struct ForecastData: Decodable {
     public let forecastday: [ForecastDay]
 }
 
-public struct ForecastDay: Decodable, Sendable, Identifiable {
+public struct ForecastDay: Decodable, Identifiable {
     public var id: String { date }
     public let date: String
     public let dateEpoch: Int
@@ -17,7 +17,7 @@ public struct ForecastDay: Decodable, Sendable, Identifiable {
     }
 }
 
-public struct DayInfo: Decodable, Sendable {
+public struct DayInfo: Decodable {
     public let maxtempC: Double
     public let mintempC: Double
     public let condition: WeatherCondition
